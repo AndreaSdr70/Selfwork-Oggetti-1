cabab8771e0d523c0d4994a835938eb13c8443d1
AndreaSdr70 <andrea@sdringola.it>
2024-07-22 15:25:21 +0200

    Selfwork Oggetti 1

---
+++ main.js
@@ -0,0 +1,20 @@
+let persona = {
+    'nome' : 'Piero',
+  'cognome' : 'Pierini',
+    'età' : '34',
+
+
+};
+
+console.log(persona);
+// persona.dati();
+
+
+
+let saluto = {
+    'frase' : 'Buon giorno',
+
+};
+
+
+console.log(saluto);
